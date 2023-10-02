71a4c6c67415f789ee6ac559bf7e942277c1e904
furkansancu <furkansancu@outlook.com>
2023-10-02 18:23:05 +0300

    added basic mechanics

--- src/components/TTT/TTT.tsx
+++ src/components/TTT/TTT.tsx
@@ -4,19 +4,36 @@ import TTTCell from '../TTTCell/TTTCell';
 function TTT (props: any) {
   let classnames = styles.TTT;
   const tileCount = 9;
-  let tiles = new Array(tileCount).fill({}).map((s, index) => <TTTCell i={index} m={s.max} a={props.a} />);
 
-  if (props.index % Math.sqrt(props.m) === 0) classnames += " " + styles.noborderleft;
-  if (props.index % Math.sqrt(props.m) === Math.sqrt(props.m) - 1) classnames += " " + styles.noborderright;
-  if (props.index < Math.sqrt(props.m)) classnames += " " + styles.nobordertop;
-  if (props.index >= props.m - Math.sqrt(props.m)) classnames += " " + styles.noborderbottom;
+  const winGame = (winner: string) => {
+    let newState = props.game;
+    newState[props.table].active = false;
+    newState[props.table].winner = winner;
+    props.setgame(newState);
+    props.checkgame();
+  }
+
+  let tiles = new Array(tileCount).fill({}).map((x, i) =>
+    <TTTCell table={props.table} index={i} game={props.game} setgame={props.setgame} turn={props.turn} setturn={props.setturn} wingame={winGame} />
+  );
+
+  if (props.table % Math.sqrt(props.game[props.table].max) === 0) classnames += " " + styles.noborderleft;
+  if (props.table % Math.sqrt(props.game[props.table].max) === Math.sqrt(props.game[props.table].max) - 1) classnames += " " + styles.noborderright;
+  if (props.table < Math.sqrt(props.game[props.table].max)) classnames += " " + styles.nobordertop;
+  if (props.table >= props.game[props.table].max - Math.sqrt(props.game[props.table].max)) classnames += " " + styles.noborderbottom;
 
   return (
-    <div className={classnames} style={{
+    <div className={classnames}>
+      <div className={styles.TTT_Winner} style={{display: props.game[props.table].winner === false ? "none" : "flex"}}>
+        {props.game[props.table].winner}
+      </div>
+      <div className={styles.TTT_Tiles} style={{
       gridTemplateColumns: `repeat(${Math.sqrt(tileCount)}, ${100 / Math.sqrt(tileCount)}%)`,
-      gridTemplateRows: `repeat(${Math.sqrt(tileCount)}, ${100 / Math.sqrt(tileCount)}%)`
+      gridTemplateRows: `repeat(${Math.sqrt(tileCount)}, ${100 / Math.sqrt(tileCount)}%)`,
+      opacity: props.game[props.table].winner === false ? 1 : 0.25
       }}>
-      {tiles}
+        {tiles}
+      </div>
     </div>
   )
 }
--- src/components/TTTCell/TTTCell.tsx
+++ src/components/TTTCell/TTTCell.tsx
@@ -3,14 +3,44 @@ import styles from './TTTCell.module.scss';
 function TTTCell(props: any) {
     let classnames = styles.TTTCell;
 
+    const clickHandler = () => {
+        if (!props.game[props.table].active) return null;
+        if (!props.game[props.table].tiles[props.index].active) return null;
+
+        let newState = props.game;
+        newState[props.table].tiles[props.index] = {value: props.turn ? "X" : "0", active: false};
+        props.setgame(newState);
+        props.setturn(!props.turn);
+        checkGame();
+    }
+
+    const checkGame = () => {
+        const winningPosibilities = [
+            [1,2,3], [4,5,6], [7,8,9],
+            [1,4,7], [2,5,8], [3,6,9],
+            [1,5,9], [3,5,7],
+        ]
+        winningPosibilities.forEach(element => {
+            let values = element.map(i => props.game[props.table].tiles[i - 1]);
+            values = values.map(i => i.value);
+            if (values.includes(null)) return null;
+            if (arrayElementsEqual(values)) {
+                props.wingame(values[0]);
+            }
+        });
+    }
+
+    const arrayElementsEqual = (array: Array<any>) => new Set(array).size === 1;
+
     return (
         <div
             className={classnames}
             style={{
-                cursor: props.a ? "pointer" : "inherit"
+                cursor: props.game[props.table].active ? "pointer" : "inherit"
             }}
+            onClick={clickHandler}
             >
-            {Math.round(Math.random()) ? "O" : "X"}
+            {props.game[props.table].tiles[props.index].value}
         </div>
     )
 }
