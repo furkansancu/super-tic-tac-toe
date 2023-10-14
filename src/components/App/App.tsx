bd2bee6494f675537fc9d8e9443ef627a7cc2d5a
furkansancu <furkansancu@outlook.com>
2023-10-14 17:14:20 +0300

    reset game dialog added.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,6 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import styles from './App.module.scss';
 
 import Game from '../Game/Game';
@@ -5,19 +8,23 @@ import Controls from '../Controls/Controls';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <div className={styles.App_Container}>
-        <div className={styles.App_Game}>
-          <Game />
-        </div>
-        <div className={styles.App_Actions}>
-          <Controls />
-        </div>
-        <div className={styles.App_Attribution}>
-          you can give a star to this project on <a href="https://github.com/furkansancu/super-tic-tac-toe" target="_blank" rel="noopener noreferrer">github.com</a>.
+    <ThemeProvider theme={createTheme({palette: {mode: 'dark'}})}>
+      <CssBaseline />
+      <div className={styles.App}>
+        <div className={styles.App_Container}>
+          <div className={styles.App_Game}>
+            <Game />
+          </div>
+          <div className={styles.App_Actions}>
+            <Controls />
+          </div>
+          <div className={styles.App_Attribution}>
+            you can give a star to this project on <a href="https://github.com/furkansancu/super-tic-tac-toe" target="_blank" rel="noopener noreferrer">github.com</a>.
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
+    
   )
 }
 
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,19 +1,60 @@
+import { useState, forwardRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { resetGame } from '../../app/gameReducer';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 
 import AutorenewIcon from '@mui/icons-material/Autorenew';
+
+const Transition = forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function Controls() {
     const dispatch = useDispatch();
 
-    const reset = () => dispatch(resetGame())
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => setOpen(true);
+
+    const handleClose = () => setOpen(false);
+
+    const reset = () => {dispatch(resetGame()); handleClose();}
 
     return (
         <Box display="flex" justifyContent="center" marginTop={4}>
-            <Button variant="contained" endIcon={<AutorenewIcon />} onClick={reset}>reset game</Button>
+            <Button variant="outlined" endIcon={<AutorenewIcon />} onClick={handleClickOpen}>reset game</Button>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Do you want to reset game ?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        This action cannot be restored.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={reset}>Reset</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     )
 }
