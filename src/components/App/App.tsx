d5375c264f3ee8630a3bf312c7566da7b27d0b3f
furkansancu <furkansancu@outlook.com>
2023-10-14 17:06:32 +0300

    attribution added.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,6 +13,9 @@ function App() {
         <div className={styles.App_Actions}>
           <Controls />
         </div>
+        <div className={styles.App_Attribution}>
+          you can give a star to this project on <a href="https://github.com/furkansancu/super-tic-tac-toe" target="_blank" rel="noopener noreferrer">github.com</a>.
+        </div>
       </div>
     </div>
   )
